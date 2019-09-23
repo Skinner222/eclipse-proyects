59398c5f63efcca391e0c0c0df2f31b17d07d929
Lobo <snowrow618@gmail.com>
2019-09-23 14:16:08 +0200

    23-08-2019

--- Tema 1/src/Ejercicios_Repaso_Clases/Ejercicio3_Main.java
+++ Tema 1/src/Ejercicios_Repaso_Clases/Ejercicio3_Main.java
@@ -1,10 +1,18 @@
 package Ejercicios_Repaso_Clases;
 
+import org.apache.log4j.Logger;
+
 public class Ejercicio3_Main {
 
+	static Logger log=Logger.getLogger(Ejercicio3_Main.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		log.info("Llamada a la funcion.");
+		log.debug("LLama funcion");
+		log.error("El ordenador arde");
+		
 	}
 
 }
